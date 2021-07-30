6860b84ec1075f69bc2232dd16f99d73f7e97777
brunEdo <edobruno.m@gmail.com>
2021-07-30 17:32:46 +0200

    dice game

--- js/app.js
+++ js/app.js
@@ -22,3 +22,51 @@ document.getElementById('close').addEventListener('click', () => {
     document.getElementById("login-overlay").classList.add("close");
 });
 
+// DICE GAME
+let userPoints = 0;
+let computerPoints = 0;
+const diceGame = () => {
+    // generate user number
+    let userNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(userNumber)
+    // generate computer number
+    let computerNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(computerNumber)
+    // check who has won
+    if (userNumber == computerNumber) {
+        // insert user score
+        document.getElementById("user-number").innerHTML = userNumber;
+        // insert computer score
+        document.getElementById("computer-number").innerHTML = computerNumber;
+        // insert games result
+        document.getElementById("match-winner").innerHTML = "Tie";
+        // update user points
+        userPoints++
+        document.getElementById("user-points").innerHTML = userPoints;
+        // update computer points
+        computerPoints++
+        document.getElementById("computer-points").innerHTML = computerPoints;
+    } else if (userNumber < computerNumber) {
+        // insert user score
+        document.getElementById("user-number").innerHTML = userNumber;
+        // insert computer score
+        document.getElementById("computer-number").innerHTML = computerNumber;
+        // insert games result
+        document.getElementById("match-winner").innerHTML = "Computer Won";
+        // update computer points
+        computerPoints++
+        document.getElementById("computer-points").innerHTML = computerPoints;
+    } else if (userNumber > computerNumber) {
+        // insert user score
+        document.getElementById("user-number").innerHTML = userNumber;
+        // insert computer score
+        document.getElementById("computer-number").innerHTML = computerNumber;
+        // insert games result
+        document.getElementById("match-winner").innerHTML = "You Won";
+        // update user points
+        userPoints++
+        document.getElementById("user-points").innerHTML = userPoints;
+    }
+}
+
+document.getElementById('start-game').addEventListener('click', diceGame);
